2f609eb6f1e24f9a23aca66849f58a35fbebd41c
Julien Bordas <julien.bordas13@gmail.com>
2019-12-30 20:19:36 +0100

    Add MultiProcessorCompile flag

--- premake5.lua
+++ premake5.lua
@@ -9,6 +9,10 @@ workspace "PepperMint"
 		"Dist"
 	}
 
+	flags {
+		"MultiProcessorCompile"
+	}
+
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 -- Include directories relative to root folder (solution directory)
