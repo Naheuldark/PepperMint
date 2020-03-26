4daf8676862ac53ef68da14041d675025758b065
Julien Bordas <julien.bordas13@gmail.com>
2020-03-26 13:18:23 +0100

    Log to PepperMint.log also

--- PepperMint/src/PepperMint/Core/Log.cpp
+++ PepperMint/src/PepperMint/Core/Log.cpp
@@ -2,6 +2,7 @@
 #include "PepperMint/Core/Log.h"
 
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace PepperMint {
 
@@ -9,12 +10,21 @@ Ref<spdlog::logger> Log::_coreLogger;
 Ref<spdlog::logger> Log::_clientLogger;
 
 void Log::Init() {
-	spdlog::set_pattern("%^[%T] %n: %v%$");
+	std::vector<spdlog::sink_ptr> logSinks;
+	logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+	logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("PepperMint.log", true));
 
-	_coreLogger = spdlog::stdout_color_mt("PEPPERMINT");
+	logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+	logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+	_coreLogger = std::make_shared<spdlog::logger>("PEPPERMINT", begin(logSinks), end(logSinks));
+	spdlog::register_logger(_coreLogger);	
 	_coreLogger->set_level(spdlog::level::trace);
+	_coreLogger->flush_on(spdlog::level::trace);
 
-	_clientLogger = spdlog::stdout_color_mt("APP");
+	_clientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+	spdlog::register_logger(_clientLogger);
 	_clientLogger->set_level(spdlog::level::trace);
+	_clientLogger->flush_on(spdlog::level::trace);
 }
 }
