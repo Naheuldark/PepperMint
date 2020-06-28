0d6cab4b87e2b6a7aa04a75bcacd39b2638cac09
Julien Bordas <julien.bordas13@gmail.com>
2020-06-28 19:06:45 +0200

    Have rotation transformed to radians from degrees (API)

--- PepperMint/src/PepperMint/Renderer/Renderer2D.cpp
+++ PepperMint/src/PepperMint/Renderer/Renderer2D.cpp
@@ -221,7 +221,7 @@ void Renderer2D::DrawQuad(const glm::vec3& iPosition,
 	}
 
 	auto&& transform = glm::translate(glm::mat4(1.0f), iPosition) *
-					   glm::rotate(glm::mat4(1.0f), iRotation, { 0.0f, 0.0f, 1.0f }) *
+					   glm::rotate(glm::mat4(1.0f), glm::radians(iRotation), { 0.0f, 0.0f, 1.0f }) *
 					   glm::scale(glm::mat4(1.0f), { iScale.x, iScale.y, 1.0f });
 
 	for (size_t i = 0; i < quadVertexCount; i++) {
--- Sandbox/src/Sandbox2D/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D/Sandbox2D.cpp
@@ -35,7 +35,7 @@ void Sandbox2D::onUpdate(PepperMint::Timestep iTimestep) {
 		PM_PROFILE_SCOPE("Renderer Draw");
 
 		static float rotation = 0.0f;
-		rotation += iTimestep * 0.5f;
+		rotation += iTimestep * 50.0f;
 
 		PepperMint::Renderer2D::BeginScene(_cameraController.camera());
 		PepperMint::Renderer2D::DrawQuad({ 1.0f, 0.0f }, -45.0f, { 0.8f, 0.8f }, 1.0f, nullptr, _squareColor);
